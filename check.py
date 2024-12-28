069855cc6572290b20b754f01acca1f49b1d9e5c
sharathdoes <justabountyhunter935@gmail.com>
2024-12-28 19:17:03 +0530

    changed name

--- check.py
+++ check.py
@@ -18,7 +18,7 @@ load_dotenv()
 # Load the GROQ and OpenAI API keys
 groq_api_key = os.getenv('GROQ_API_KEY')
 
-st.title("Chatgroq With Llama3 Demo")
+st.title("Operating systems")
 
 # Define the LLM
 llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
